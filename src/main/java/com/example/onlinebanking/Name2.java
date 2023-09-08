5caa1a8963e4dd56160a6526115511bc99cf8650
man250001 <monizcr@oldcolony.info>
2023-09-08 09:40:04 -0400

    Add New Fxml files

--- src/main/java/com/example/onlinebanking/Name2.java
+++ src/main/java/com/example/onlinebanking/Name2.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Name2 extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Name2.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Name2.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/example/onlinebanking/SavingsAccount.java
@@ -0,0 +1,4 @@
+package com.example.onlinebanking;
+
+public class SavingsAccount {
+}
