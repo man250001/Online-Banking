32181ab31c75ee77e2568cfe639423536ab61667
man250001 <monizcr@oldcolony.info>
2023-09-29 11:20:44 -0400

    make a user constructor

--- src/main/java/com/example/onlinebanking/User.java
+++ src/main/java/com/example/onlinebanking/User.java
@@ -1,7 +1,15 @@
 package com.example.onlinebanking;
 
+@SuppressWarnings("unused")
 public class User {
 
+        String firstname, lastname;
+        int userid;
+        //Accounts variable :)
 
-
+        public User(String firstname, String lastname, int userid) {
+                this.firstname = firstname;
+                this.lastname = lastname;
+                this.userid = userid;
+        }
 }
