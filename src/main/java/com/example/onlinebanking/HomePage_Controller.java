70edd7dda2dfc63d4b18b1c77a702cd7692ab96b
man250001 <monizcr@oldcolony.info>
2023-10-04 12:08:50 -0400

    Style all pages using css

--- src/main/java/com/example/onlinebanking/HomePage_Controller.java
+++ src/main/java/com/example/onlinebanking/HomePage_Controller.java
@@ -3,7 +3,7 @@ package com.example.onlinebanking;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Hyperlink;
-
+import javafx.scene.control.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,13 +12,20 @@ public class HomePage_Controller implements Initializable {
     @FXML
     private Hyperlink checkingaccount_link, payBills_link, savingsaccount_link, signout_link, transfer_link, admin_link;
 
+    @FXML
+    private Label balance_label;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        balance_label.setText("Balance: $" + DBUtils.getUser().getBalance());
         checkingaccount_link.setOnAction(event -> DBUtils.changeScene(event, "checkingAccount.fxml", "Checking Account", null, null));
         payBills_link.setOnAction(event -> DBUtils.changeScene(event, "payBills.fxml", "Pay Bills", null, null));
         savingsaccount_link.setOnAction(event -> DBUtils.changeScene(event, "savingsAccount.fxml", "Savings Account", null, null));
-        signout_link.setOnAction(event -> DBUtils.changeScene(event, "login.fxml", "Login", null, null));
+        signout_link.setOnAction(event -> {
+            DBUtils.getUser().signOutUser();
+            DBUtils.changeScene(event, "login.fxml", "Login", null, null);
+        });
         transfer_link.setOnAction(event -> DBUtils.changeScene(event, "transferFunds.fxml", "Transfer Funds", null, null));
-        admin_link.setOnAction(event -> DBUtils.changeScene(event, "admin.fxml", "Admin", null, null));
+        admin_link.setOnAction(event -> DBUtils.changeScene(event, "adminPage.fxml", "Admin", null, null));
     }
 }
